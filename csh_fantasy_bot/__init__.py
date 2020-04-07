d9e013b36e65d113e1a5abe6ffac9d4e8728157a
Craig Hamilton <craigh@quailholdings.com>
2020-04-07 09:08:53 -0700

    log broker url for rabbit

--- csh_fantasy_bot/__init__.py
+++ csh_fantasy_bot/__init__.py
@@ -1,11 +1,14 @@
 import os
+import logging
 from celery import Celery
 
 rabbit_url = os.environ.get('CELERY_BROKER_URL', 'amqp://guest:guest@localhost:5672')
 
 def make_celery(app_name=__name__):
+    """ Connect celery to broker."""
     backend = "redis://localhost:6379/0"
     broker = rabbit_url
+    logging.info(f"Celery connecting to rabbit broker {broker}")
     return Celery(app_name, broker=broker) # backend=backend,
 
 celery = make_celery()
