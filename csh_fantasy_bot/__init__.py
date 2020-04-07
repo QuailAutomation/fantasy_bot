7f28f378b056dfe845a79a07cb47b0b830254633
Craig Hamilton <craigh@quailholdings.com>
2020-04-07 09:32:41 -0700

    add beat schedule

--- csh_fantasy_bot/__init__.py
+++ csh_fantasy_bot/__init__.py
@@ -4,11 +4,22 @@ from celery import Celery
 
 rabbit_url = os.environ.get('CELERY_BROKER_URL', 'amqp://guest:guest@localhost:5672')
 
+
 def make_celery(app_name=__name__):
-    """ Connect celery to broker."""
+    """Connect celery to broker."""
     backend = "redis://localhost:6379/0"
     broker = rabbit_url
     logging.info(f"Celery connecting to rabbit broker {broker}")
+    app.conf.beat_schedule = {
+  # 'refresh': {
+  #   'task': 'refresh',
+  #   'schedule': 60
+  # },
+    'check_roster_moves': {
+    'task': 'check_roster_moves',
+    'schedule': 300
+  },
+}
     return Celery(app_name, broker=broker) # backend=backend,
 
 celery = make_celery()
