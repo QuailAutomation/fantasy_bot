ccd4622f3529673c72534cf8f99acf115f23a34d
Craig Hamilton <craigh@quailholdings.com>
2020-04-26 18:52:39 -0700

    remove old files

--- parser_test.py
+++
@@ -1,8 +0,0 @@
-from csh_fantasy_bot import fantasysp_scrape
-import pandas as pd
-
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 1000)
-
-# p = fantasysp_scrape.Parser(positions=['G'])
-p = fantasysp_scrape.Parser()
