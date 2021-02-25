8696c0fd32caac67132c8cd6df50c40358a57ff2
Craig Hamilton <craigh@quailholdings.com>
2021-02-25 15:08:31 -0800

    web scraping now uses remote chrome container

--- Dockerfile
+++ Dockerfile
@@ -15,6 +15,15 @@ RUN git clone https://github.com/APMonitor/apm_server.git
 
 
 FROM python:3.8-slim AS build-image
+RUN apt-get update
+RUN apt-get install -y gnupg2 libnss3 libgconf-2-4 wget
+
+#RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+# RUN echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | tee /etc/apt/sources.list.d/google-chrome.list
+RUN wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+RUN apt install -y ./google-chrome-stable_current_amd64.deb
+#RUN apt-get install -y google-chrome-stable
+
 COPY --from=compile-image /opt/venv /opt/venv
 COPY --from=compile-image /apm_server/apm_linux/bin/apm /usr/local/bin
 RUN chmod a+x /usr/local/bin/apm
@@ -22,7 +31,7 @@ RUN chmod a+x /usr/local/bin/apm
 ENV PATH="/opt/venv/bin:$PATH"
 
 WORKDIR /app
-COPY requirements.txt .
+# COPY requirements.txt .
 COPY library ./library
 
 
--- csh_fantasy_bot/yahoo_scraping.py
+++ csh_fantasy_bot/yahoo_scraping.py
@@ -5,7 +5,10 @@ from enum import Enum
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
-
+try: 
+    import chromedriver_binary 
+except ImportError:
+    pass
 import time, re, csv, sys, random
 
 from csh_fantasy_bot.league import FantasyLeague
@@ -106,11 +109,34 @@ class YahooProjectionScraper:
         # projection_length = "S_PS14" # S_PS7 or S_PSR
 
         chrome_options = Options()
-        chrome_options.add_extension('chrome-ublock.crx')
-        chrome_options.add_argument("--enable-extensions")
+        # chrome_options.add_extension('chrome-ublock.crx')
+        # chrome_options.add_argument("--enable-extensions")
+    
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        # chrome_options.add_argument("window-size=1400,2100") 
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument('--disable-background-timer-throttling')
+        chrome_options.add_argument('--disable-breakpad')
+        chrome_options.add_argument('--disable-component-extensions-with-background-pages')
+        chrome_options.add_argument('--disable-extensions')
+        # chrome_options.add_argument('')
+        chrome_options.add_argument('--disable-features=TranslateUI,BlinkGenPropertyTrees')
+        chrome_options.add_argument('--disable-ipc-flooding-protection')
+        chrome_options.add_argument('--disable-renderer-backgrounding')
+        chrome_options.add_argument('--enable-features=NetworkService,NetworkServiceInProcess')
+        chrome_options.add_argument('--force-color-profile=srgb')
+        chrome_options.add_argument('--hide-scrollbars')
+        chrome_options.add_argument('--metrics-recording-only')
+        chrome_options.add_argument('--mute-audio')
+        chrome_options.add_argument("--disable-logging")
+        chrome_options.add_argument("--log-level=3")
 
-        driver = webdriver.Chrome(chrome_options=chrome_options)
-        driver.set_page_load_timeout(30)
+        driver = webdriver.Remote("http://192.168.1.20:3001/webdriver", chrome_options.to_capabilities())
+        
+        # driver = webdriver.Chrome(chrome_options=chrome_options)
+        driver.set_page_load_timeout(6000)
 
         print("Logging in")
         self.login(driver)
